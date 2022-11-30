3810f705f24eb15e86476dbd977059e2a1db3be6
sasisrinivasa1 <ssasikumar9651@gmail.com>
2022-11-30 08:16:29 +0530

    Updated Practiced programs

---
+++ simple_program.py
@@ -0,0 +1,9 @@
+number1= 10
+number2= 234
+number3= 126
+number= number1+number2+number3
+
+def printTotal():
+    print("Total Number is: ", number)
+print("What is the Total Number?")
+printTotal()
---
+++ simple_program_params.py
@@ -0,0 +1,11 @@
+import sys
+
+def Params(Value): 
+    print("The Input is", Value)
+
+print("Hello India", "\nWhat is the input")
+
+
+# predefined main function
+if __name__ == "__main__":
+    Params(sys.argv[1]) # sys.argv[1] gets value by passing parameter
