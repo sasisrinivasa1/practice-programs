7f8416d72f836a6a52b0d5102788f26cbb4becfc
sasisrinivasa1 <ssasikumar9651@gmail.com>
2022-12-07 12:38:51 +0530

    New program

---
+++ data_types/Strings/if_statements.py
@@ -0,0 +1,11 @@
+def mobile(brand):
+    print("The Brand is ", brand)
+
+brands = "Samsung"
+
+if brands.find("oneplus") == -1:
+    if brands.find("Samsung") != -1:
+        mobile("Samsung")
+    else: 
+        print("No brands found")
+else: print("No brands found")
