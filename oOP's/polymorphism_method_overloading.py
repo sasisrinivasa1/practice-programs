98e06f0dccc4e166138e06bb6162561b30c94978
sasisrinivasa1 <ssasikumar9651@gmail.com>
2022-12-29 23:52:27 +0530

    Updated Oops and function program

---
+++ oOP's/polymorphism_method_overloading.py
@@ -0,0 +1,17 @@
+class MyClass():
+
+    def add(self,a,b,c):
+        self.a = a
+        self.b = b
+        self.c = c
+        return self.a + self.b + self.c
+    def add(self,a,b):
+        self.a = a
+        self.b = b
+        return self.a + self.b
+
+
+obj = MyClass()
+
+# print(obj.add(2,3,5))
+print(obj.add(2,3)) # Latest defined method will run by default
